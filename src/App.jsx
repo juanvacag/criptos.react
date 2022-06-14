27537c897e4bdd22d79983e4beeafff1f20c9a5a
juanvacag <>
2022-06-14 17:21:08 +0200

    componente error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -50,6 +51,7 @@ function App() {
 
         <div>
             <Heading>Cotiza Criptomonedas al Instante</Heading>
+            <Formulario />
         </div>
     </Contenedor>
   )
